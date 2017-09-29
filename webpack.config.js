b26ce7e2894b58200eec12a6149393531c6719c1
Alexander <lss1ash@rambler.ru>
2017-09-29 23:28:07 +0300

    настройка webpack-a

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          'eslint-loader',
-          'babel-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [new HtmlWebpackPlugin()]
+	entry: './source/client/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					'eslint-loader',
+					'babel-loader'
+				]
+			}
+			// ,
+			// {
+			// 	test: /\.css$/,
+			// 	use: [
+			// 		'style-loader',
+			// 		'css-loader',
+			// 		'postcss-loader'
+			// 	]
+			// }
+		]
+	}
+	// ,
+	// plugins: [new HtmlWebpackPlugin({
+	// 	title: 'Yamoney Node.js School'
+	// })]
 };
