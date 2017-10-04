157277a9970dbf737011f6a482a1062960a3efbb
Alexander <lss1ash@rambler.ru>
2017-10-04 22:23:41 +0300

    Текущая ситуация с SSR

--- source/app.js
+++ source/app.js
@@ -7,7 +7,7 @@ const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser')();
 // const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const App = require('../public/bundle');
+const App = require('../public/bundle').default;
 
 const getCardsController = require('./controllers/cards/get-cards');
 const createCardController = require('./controllers/cards/create');
@@ -29,7 +29,7 @@ router.param('id', (id, ctx, next) => next());
 
 router.get('/', (ctx) => {
 	// ctx.body = fs.readFileSync('./public/index.html', 'utf8');
-	let test = ReactDOMServer.renderToString(App);
+	let test = ReactDOMServer.renderToString(App());
 	console.log(typeof App);
 	ctx.body = test;
 	console.log(test);
--- source/server/index.js
+++ source/server/index.js
@@ -1,1 +1,8 @@
-export {default as App} from '../client/components/App';
+// export {default as App} from '../client/components/App';
+
+import React from 'react';
+import {App} from '../client/components';
+
+export default () => {
+ return <App />;
+};
