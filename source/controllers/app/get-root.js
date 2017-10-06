df529a56b71605a20c0eabb0d272f7333c909c8a
Alexander <lss1ash@rambler.ru>
2017-10-07 00:35:30 +0300

    Root правлен

--- source/controllers/app/get-root.js
+++ source/controllers/app/get-root.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const {file} = require('../../libs/');
+// const file = require('../../libs/file');
+const fs = require('fs');
 const path = require('path');
 
-module.exports = async (ctx) => {
-	ctx.body = await file.read(path.join(__dirname, '../../views/client/index.html'));
+module.exports = (ctx) => {
+	ctx.body = fs.readFileSync(path.join(__dirname, '../../views/client/index.html'), 'utf-8');
 };
