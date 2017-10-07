7b022c78cab4a7f8a4ab27ee4f4d4673d2a700f4
Alexander <lss1ash@rambler.ru>
2017-10-08 02:54:39 +0300

    Отказались от минификации тела отдаваемого html - несоответсвия с серверной разметкой

--- source/controllers/app/get-root.js
+++ source/controllers/app/get-root.js
@@ -7,6 +7,7 @@ const ssr = require('../../views/server/bundle');
 module.exports = (ctx) => {
 	const template = fs.readFileSync(path.join(__dirname, '../../views/client/index.html'), 'utf-8');
 	const {html, css, data} = ssr();
+
 	ctx.body = template
 		.replace('{{html}}', html)
 		.replace('{{css}}', css)
