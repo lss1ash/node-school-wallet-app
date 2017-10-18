7dc6b96e4f1f2202e86b3f7dd6325ed6044ccd53
Alexander <lss1ash@rambler.ru>
2017-10-19 01:27:44 +0300

    Формат времени изменён

--- source/models/transactions.js
+++ source/models/transactions.js
@@ -41,11 +41,7 @@ class Transactions {
 		if (isDataValid) {
 			transaction.id = this._transactions.length + 1;
 			transaction.cardId = Number(transaction.cardId);
-			const now = new Date();
-
-			// Добавить getTimezoneOffset()
-			transaction.time = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}` +
-				`T${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+			transaction.time = Transactions.getTime();
 			this._transactions.push(transaction);
 
 			await file.write(this._dataSource, this._transactions);
@@ -58,6 +54,33 @@ class Transactions {
 
 	static remove() {
 		throw new ApplicationError('Card removing is prohibited!', 400);
+	}
+
+	// formay: 2017-08-9T05:28:31+03:00
+	static getTime() {
+		const now = new Date();
+		const YYYY = now.getFullYear();
+		let MM = now.getMonth() + 1;
+		let DD = now.getDate();
+		let HH = now.getHours();
+		let MI = now.getMinutes();
+		let SS = now.getSeconds();
+		MM = MM < 10 ? `0${MM}` : MM;
+		DD = DD < 10 ? `0${DD}` : DD;
+		HH = HH < 10 ? `0${HH}` : HH;
+		MI = MI < 10 ? `0${MI}` : MI;
+		SS = SS < 10 ? `0${SS}` : SS;
+
+		// Timezone
+		let TZ = -now.getTimezoneOffset() / 60;
+		const sign = TZ < 0 ? '-' : '+';
+		TZ = Math.abs(TZ);
+		let TZ_HH = Math.floor(TZ);
+		let TZ_MM = (TZ - TZ_HH) * 60;
+		TZ_HH = TZ_HH < 10 ? `0${TZ_HH}` : TZ_HH;
+		TZ_MM = TZ_MM < 10 ? `0${TZ_MM}` : TZ_MM;
+
+		return `${YYYY}-${MM}-${DD}T${HH}:${MI}:${SS}${sign}${TZ_HH}:${TZ_MM}`;
 	}
 
 	// eslint-disable-next-line
